9678fc77d280e2004ee9acae5894e6dab118b467
iSmash <nemeth.richo@gmail.com>
2019-04-19 12:59:57 +0100

    test for message if no article has been created yet.

--- spec/feature/listing_article_spec.rb
+++ spec/feature/listing_article_spec.rb
@@ -12,6 +12,7 @@ RSpec.feature "Listing Articles" do
 	scenario "A user lists all articles" do
 
 		visit "/"
+
 		expect(page).to have_content(@article1.title) 
 		expect(page).to have_content(@article1.body) 
 		expect(page).to have_content(@article2.title) 
@@ -19,5 +20,25 @@ RSpec.feature "Listing Articles" do
 		expect(page).to have_link(@article1.title) 
 		expect(page).to have_link(@article2.title)
     end
+
+    scenario "A user has no articles" do
+    	
+    	Article.delete_all
+
+		visit "/"
+
+		expect(page).not_to have_content(@article1.title) 
+		expect(page).not_to have_content(@article1.body) 
+		expect(page).not_to have_content(@article2.title) 
+		expect(page).not_to have_content(@article2.body) 
+		expect(page).not_to have_link(@article1.title) 
+		expect(page).not_to have_link(@article2.title)
+
+		within("h1#no-articles") do #specify what content has to shows the expectation inside the block
+			expect(page).to have_content("No Article Created")
+		end
+    end
+
+
 
 end
