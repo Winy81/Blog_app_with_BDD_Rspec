48832b25801f94442a88cb4fee1a9cb72becfc78
iSmash <nemeth.richo@gmail.com>
2019-04-19 11:26:15 +0100

    styling for the list of article. Scope for ordering of articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
 	validates_presence_of :title, message: "Title can't be blank"
 	validates_presence_of :body, message: "Body can't be blank"
+
+	default_scope { order(created_at: :desc)}
 end
