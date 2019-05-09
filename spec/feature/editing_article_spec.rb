3eeead762b8572e7ce355c2794fbd38bbf3a081a
iSmash <nemeth.richo@gmail.com>
2019-05-09 12:47:44 +0100

    editing article test for scenario: update has been failed

--- spec/feature/editing_article_spec.rb
+++ spec/feature/editing_article_spec.rb
@@ -20,4 +20,18 @@ RSpec.describe "Editing an articles" do
 		expect(page.current_path).to eq(article_path(@article))
 	end
 
+	scenario "A user fails updates an article" do 
+		visit "/" #root path
+
+		click_link @article.title
+		click_link "Edit Article"
+
+		fill_in "Title", with: ""
+		fill_in "Body", with: "Updated Body io Article"
+		click_button "Update Article"
+
+		expect(page).to have_content("Article has not been updated")
+		expect(page.current_path).to eq(article_path(@article))
+	end
+
 end
