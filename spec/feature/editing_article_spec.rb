9c11031b8400f61e623479b4d6588fade105d94a
iSmash <nemeth.richo@gmail.com>
2019-05-09 12:41:36 +0100

    editing article test for scenario: user update article

---
+++ spec/feature/editing_article_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "Editing an articles" do 
+
+	before do 
+		@article = Article.create(title: "Title one", body: "Body of article one")
+	end
+
+	scenario "A user updates an article" do 
+		visit "/" #root path
+
+		click_link @article.title
+		click_link "Edit Article"
+
+		fill_in "Title", with: "Updated Title"
+		fill_in "Body", with: "Updated Body io Article"
+		click_button "Update Article"
+
+		expect(page).to have_content("Article has been updated")
+		expect(page.current_path).to eq(article_path(@article))
+	end
+
+end
