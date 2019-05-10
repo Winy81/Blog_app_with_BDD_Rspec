27612cd5e9eaf1f1f3b97e0c3d248a2e009a508a
iSmash <nemeth.richo@gmail.com>
2019-05-10 18:31:05 +0100

    make the article delete test passed. Setup delete action, implement link into the show page

--- spec/feature/delete_article_spec.rb
+++ spec/feature/delete_article_spec.rb
@@ -13,7 +13,7 @@ RSpec.feature "Deleting an article" do
 		click_link "Delete Article"
 
 		expect(page).to have_content("Article has been deleted")
-		expect(current_path).to eq(article_path)
+		expect(current_path).to eq(articles_path)
 	end
 
 end
