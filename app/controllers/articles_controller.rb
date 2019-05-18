01f82809f0f3d5f70eb493cfed819e665ef2e27b
iSmash <nemeth.richo@gmail.com>
2019-05-18 13:17:22 +0100

    article before action for user authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  
+  #before_action :authenticate_user!, except: [:index, :show]
   before_action :set_article, only:[:show,:edit,:update,:destroy] #filtering @article = Article.find(params[:id]) line of code what is includeed, :show,:edit,:update,:destroy action. We can setup a method for them to keep the code DRY
 
   def index
