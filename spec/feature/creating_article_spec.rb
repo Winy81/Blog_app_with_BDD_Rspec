a5c19d70687c85a1f1553fb36c6c042db6cbea17
iSmash <nemeth.richo@gmail.com>
2019-05-20 16:18:07 +0100

    setup current_user in the article controller, make the creating_article test be passed

--- spec/feature/creating_article_spec.rb
+++ spec/feature/creating_article_spec.rb
@@ -13,9 +13,10 @@ RSpec.feature "Creating Articles" do
 		fill_in "Title", with: "Creating a blog"
 		fill_in "Body", with: "Lorem Ipsum"
 		click_button "Create Article"
+		expect(Article.last.user).to eq(@john)
 		expect(page).to have_content("Article has been created")
 		expect(page.current_path).to eq(articles_path)
-		expect(page).to have_content("created by: #{@john.email}") 
+		expect(page).to have_content("Created by: #{@john.email}") 
 	end
 
 	scenario "A user fails to create a new article" do
