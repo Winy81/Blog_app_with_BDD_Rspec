9e1890ec76dc46f039d2f0d0b726616210772de7
iSmash <nemeth.richo@gmail.com>
2019-05-22 14:49:18 +0100

    test refactor for listing article when the user signed in

--- spec/feature/listing_article_spec.rb
+++ spec/feature/listing_article_spec.rb
@@ -3,11 +3,11 @@ require "rails_helper"
 RSpec.feature "Listing Articles" do
 
 	before do
-		john = User.create(email: "john@test.com", password: "password")
+		@john = User.create(email: "john@test.com", password: "password")
 		@article1 = Article.create(title: "The first article",
-		body: "Lorem ipsum dolor sit amet, consectetur.", user: john) 
+		body: "Lorem ipsum dolor sit amet, consectetur.", user: @john) 
 		@article2 = Article.create(title: "The second article",
-		body: "Pellentesque ac ligula in tellus feugiat.", user: john) 
+		body: "Pellentesque ac ligula in tellus feugiat.", user: @john) 
 	end
 
 	scenario "with articles created and user not signed in" do
@@ -22,6 +22,20 @@ RSpec.feature "Listing Articles" do
 		expect(page).to have_link(@article2.title)
 		expect(page).not_to have_link("New Article")
     end
+
+    scenario "with articles created and user signed in" do
+    	login_as(@john)
+		visit "/"
+
+		expect(page).to have_content(@article1.title) 
+		expect(page).to have_content(@article1.body) 
+		expect(page).to have_content(@article2.title) 
+		expect(page).to have_content(@article2.body) 
+		expect(page).to have_link(@article1.title) 
+		expect(page).to have_link(@article2.title)
+		expect(page).to have_link("New Article")
+    end
+
 
     scenario "A user lists all articles" do
 
