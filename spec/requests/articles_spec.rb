7c2b33a4dd8f9536b6ac82be957d71b22450fcb5
iSmash <nemeth.richo@gmail.com>
2019-06-11 19:37:20 +0100

    test users has been setup in article_spec.rb for test for preflitering in the controller

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Articles", type: :request do 
 
-	before do 
-		@article = Article.create(title: "Title one", body: "Body of article one")
+	before do
+		@john = User.create(email: "john@test.com", password: "password")
+		@jane = User.create(email: "jane@test.com", password: "password")
+		@article = Article.create!(title: "Title one", body: "Body of article one", user: @john)
 	end
 
 
