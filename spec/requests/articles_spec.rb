4ddbf7494034dd90771eeba2a9f4d58f4de5062c
iSmash <nemeth.richo@gmail.com>
2019-06-12 19:37:41 +0100

    test for article controller edit and update with the article owner

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -30,4 +30,24 @@ RSpec.describe "Articles", type: :request do
 			end 
 		end
 	end
+
+	describe 'GET /articles/:id' do 
+		context 'with existing article' do 
+			before { get "/articles/#{@article.id}"}
+
+			it " has handle existing article" do 
+				expect(response.status).to eq(200)
+			end
+		end
+
+		context 'with non-existing article' do 
+			before { get "/articles/radnom_not_exist"}
+
+			it 'has handle non-existing article' do 
+				expect(response.status).to eq(302)
+				flash_message = "The article couldn't be found"
+				expect(flash[:alert]).to eq(flash_message)
+			end
+		end
+	end
 end
