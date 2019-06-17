97be156757981cd5f68c024c27021b81cfe6d987
iSmash <nemeth.richo@gmail.com>
2019-06-17 16:43:14 +0100

    comment#create action has been created

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+
+	def create
+	  @comment = @article.comments.new
+	  @comment.user = current_user
+
+	  if comment.save
+	    flash[:notice] = "Comment has been created"
+	  else
+	  	flash.now[:alert] = "Comment hasn't been created"
+	  end
+	  redirect_to article_path(@article)
+	end
+
 end
