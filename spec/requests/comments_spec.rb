15fa4c59a835e9514507174142c25c3a871238f7
iSmash <nemeth.richo@gmail.com>
2019-06-20 18:34:15 +0100

    first test for comments controller for non signed in users

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,11 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "Articles", type: :request do 
+RSpec.describe "Comments", type: :request do 
 
 	before do
 		@john = User.create(email: "john@test.com", password: "password")
 		@jane = User.create(email: "jane@test.com", password: "password")
 		@article = Article.create!(title: "Title one", body: "Body of article one", user: @john)
+	end
+
+	describe 'POST /articles/:id/comments' do 
+		context 'with a non-signed in user' do
+		  before do
+			post "/articles/#{@article.id}/comments", params: { comment: {body: "Awesome blog."} } 
+		  end
+
+		  it "redirects user to the signin page" do 
+			flash_message = "Please sign in or sign up first"
+			expect(response).to redirect_to(new_user_session_path) 
+			expect(response.status).to eq 302 
+			expect(flash[:alert]).to eq flash_message
+		  end
+		end 
 	end
 
 end
