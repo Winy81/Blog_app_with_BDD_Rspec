596fc0303137748d054fd02bdbc6c9188a15dd63
iSmash <nemeth.richo@gmail.com>
2019-06-20 18:56:17 +0100

    test for comments controller for signed in users

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -20,7 +20,21 @@ RSpec.describe "Comments", type: :request do
 			expect(response.status).to eq 302 
 			expect(flash[:alert]).to eq flash_message
 		  end
-		end 
+		end
+
+		context 'with a logged in user' do 
+		  before do
+			login_as(@fred)
+  			post "/articles/#{@article.id}/comments", params: { comment: {body: "Awesome blog."} }
+  		  end
+
+		  it "creates the comment successfully" do 
+			 flash_message = "Comment has been created"
+			 expect(response).to redirect_to(article_path(@article.id)) 
+			 expect(response.status).to eq 302 
+			 expect(flash[:notice]).to eq flash_message
+		  end
+		end
 	end
 
 end
