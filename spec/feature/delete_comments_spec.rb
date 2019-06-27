5fe27e6f07edcb3171f80081317f1172266841bb
iSmash <nemeth.richo@gmail.com>
2019-06-27 15:47:04 +0100

    delete_comments_sepc refactor for own comment deleting

--- spec/feature/delete_comments_spec.rb
+++ spec/feature/delete_comments_spec.rb
@@ -5,35 +5,20 @@ RSpec.feature "Delete comment" do
 		@john = User.create(email: "john@test.com", password: "password")
 		@jane = User.create(email: "jane@test.com", password: "password")
 		@article = Article.create!(title: "Title one", body: "Body of article one", user: @john)
-		@comment_1 = @article.comments.create(body: "first comment")
-		@comment_2 = @article.comments.create(body: "sec comment")		
+		@comment_1 = @article.comments.create!(body: "first comment", user: @jane)
+		@comment_2 = @article.comments.create!(body: "second comment", user: @john)		
 	end
 
-	scenario "delete only last comment created by secondary user" do
+	scenario "user delete her own comment" do
 		login_as(@jane)
 
 		visit "/"
 
 		click_link @article.title
-		comment_jane_1 = @article.comments.create(body: "first comment by jane")
-		comment_jane_2 = @article.comments.create(body: "sec comment by jane")
-		click_button "Remove My Last Comment"
-		expect(page).to have_content(@comment_1) 
-		expect(page).to have_content(@comment_2)
-		expect(page).to have_content(comment_jane_1)
-		expect(page).not_to have_content(comment_jane_2)
+		click_on("Remove Comment",match: :first)
+		expect(page).not_to have_content(@comment_1.body)
+		expect(page).to have_content(@comment_2.body)
 		expect(current_path).to eq(article_path(@article))
 	end
 
-	scenario "delete only last comment created by primary user" do
-		login_as(@john)
-
-		visit "/"
-
-		click_link @article.title
-		click_button "Remove My Last Comment"
-		expect(page).to have_content(@comment_1)
-		expect(page).to have_content(@comment_2)
-		expect(current_path).to eq(article_path(@article)) 
-	end 
 end
